ac094a9bc6c3ebaf5a4b379229b086a45d0b9972
Calvin Vo <calvinctvodesign@gmail.com>
2019-08-16 10:29:55 -0700

    Updates

---
+++ precache-manifest.fb8df0eb37ce5c67190278b5348b2a57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a3fba68329caf06f893ddf977cb7237",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "592eb4d7bf423578600d",
+    "url": "/ClickyGame/static/css/main.41991a8f.chunk.css"
+  },
+  {
+    "revision": "efe8c99981abf5e57078",
+    "url": "/ClickyGame/static/js/2.f9027221.chunk.js"
+  },
+  {
+    "revision": "592eb4d7bf423578600d",
+    "url": "/ClickyGame/static/js/main.6706e56a.chunk.js"
+  },
+  {
+    "revision": "a79552d885417700486e",
+    "url": "/ClickyGame/static/js/runtime~main.e4a1a25c.js"
+  }
+]);
